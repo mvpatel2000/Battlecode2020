de72f18099b165e1b9c5b1aef56a0010b75a15ba
Vinjai Vale <vinjai.vale@gmail.com>
2020-01-09 20:17:23 -0800

    Fix prep submission script

--- scripts/prep_submission.py
+++ scripts/prep_submission.py
@@ -6,16 +6,17 @@ import os
 
 if __name__ == '__main__':
 	bot_dir = Path('./src/') / sys.argv[1]
-	submit = Path('./src/submit')
-	if submit.exists():
-		shutil.rmtree(submit)
-	submit.mkdir()
+	smite = Path('./src/smite')
+	if smite.exists():
+		shutil.rmtree(smite)
+	smite.mkdir()
 	for f in bot_dir.iterdir():
-		out = submit / f.name
+		out = smite / f.name
 		Path.touch(out)
 		with open(f, 'r') as file:
 			content = file.read()
 		out_content = re.sub('System.out.println', '//System.out.println', content)
+		out_content = re.sub('package [^\n]*;', 'package smite;', out_content)
 		with open(out, 'w') as out_file:
 			out_file.write(out_content)
-	os.system('zip -r ./src/submit.zip ./src/submit')
+	os.system('zip -r ./src/smite.zip ./src/smite')
