553b0a24e0522faf09c9ee6ada68ffecec30f318
Nikhil <sardana@protonmail.com>
2020-01-10 19:36:54 -0800

    make comms easier to read, add high-level read

--- src/sprint/SoupMessage.java
+++ src/sprint/SoupMessage.java
@@ -7,19 +7,39 @@ public class SoupMessage extends Message {
 
     Message m;
     final int soupSchema = 1;   //SoupMessages are message type 1
+    int tile;
+    int soupThere;
 
-    public SoupMessage(int myMAP_HEIGHT, int myMAP_WIDTH, int myTeam) {
-        super(myMAP_HEIGHT, myMAP_WIDTH, myTeam);
+    public SoupMessage(int myMapHeight, int myMapWidth, int myTeam) {
+        super(myMapHeight, myMapWidth, myTeam);
         this.writeSchema(soupSchema);
+        tile = -1;
+        soupThere = -1;
+    }
+
+    //Use for recieved message.
+    //use .tile and .soupThere to get information
+    public SoupMessage(int[] recieved, int myMapHeight, int myMapWidth, int myTeam) {
+        super(recieved, myMapHeight, myMapWidth, myTeam);
+        tile = -1;
+        soupThere = -1;
+        readTile();
     }
 
 
-    boolean writeTile(int tile) {
-        return writeToArray(tile, 6);
+    boolean writeTile(int myTile) {
+        tile = myTile;
+        return writeToArray(myTile, 6);
     }
 
     boolean writeSoupOrNot(int presence) {
+        soupThere = presence;
         return writeToArray(presence, 1);
+    }
+
+    void readTile() {
+        tile = readFromArray(headerLen + schemaLen + , 6);
+        soupThere = readFromArray(headerLen + schemaLen +6, 1);
     }
 
 }
