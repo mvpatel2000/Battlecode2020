aa0b003fed003f2e12e7138d24b14f51e2762d59
Nikhil <sardana@protonmail.com>
2020-01-19 00:01:03 -0800

    add terraform message

--- src/seeding/Miner.java
+++ src/seeding/Miner.java
@@ -43,7 +43,7 @@ public class Miner extends Unit {
         super(rc);
 
         aggro = rc.getRoundNum() == 2;
-//        aggro = false; // uncomment to disable aggro
+        aggro = false; // uncomment to disable aggro
         aggroDone = false;
         if (aggro) {
             target = new ArrayList<>();
