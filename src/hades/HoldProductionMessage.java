8fe1862c9cb625e0354f54d0ea266044e5eab476
Vinjai Vale <vinjai.vale@gmail.com>
2020-01-19 13:44:54 -0800

    hades

---
+++ src/hades/HoldProductionMessage.java
@@ -0,0 +1,31 @@
+package hades;
+
+public class HoldProductionMessage extends Message {
+
+    final int prodMessage = 3;   //HoldProductionMessages are message type 3
+    int enemyHQTile;
+    int bitsPerTile = 9;
+
+    public HoldProductionMessage(int myMapHeight, int myMapWidth, int myTeam) {
+        super(myMapHeight, myMapWidth, myTeam);
+        this.writeSchema(prodMessage);
+        enemyHQTile = -1;
+    }
+
+    //Use for recieved message.
+    public HoldProductionMessage(int[] recieved, int myMapHeight, int myMapWidth, int myTeam) {
+        super(recieved, myMapHeight, myMapWidth, myTeam);
+        this.schema=prodMessage;
+        readEnemyHQTile();
+    }
+
+    boolean writeEnemyHQTile(int tileNum) {
+        enemyHQTile = tileNum;
+        return writeToArray(tileNum, bitsPerTile);
+    }
+
+    void readEnemyHQTile() {
+        enemyHQTile = readFromArray(headerLen + schemaLen, bitsPerTile);
+    }
+
+}
