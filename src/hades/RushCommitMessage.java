2a8042ef97ef54569af2fe69b9b885dd12bdbf6b
Vinjai Vale <vinjai.vale@gmail.com>
2020-01-20 15:40:23 -0800

    full hades

---
+++ src/hades/RushCommitMessage.java
@@ -0,0 +1,31 @@
+package hades;
+
+public class RushCommitMessage extends Message {
+
+    final int rSchema = 7;   //BuiltMessages are message type 5
+    int typeOfCommit; // 1 is rush commit standard
+    int bitsPerType = 3;
+
+    public RushCommitMessage(int myMapHeight, int myMapWidth, int myTeam) {
+        super(myMapHeight, myMapWidth, myTeam);
+        this.writeSchema(rSchema);
+        typeOfCommit = -1;
+    }
+
+    //Use for recieved message.
+    public RushCommitMessage(int[] recieved, int myMapHeight, int myMapWidth, int myTeam) {
+        super(recieved, myMapHeight, myMapWidth, myTeam);
+        this.schema=rSchema;
+        readTypeOfCommit();
+    }
+
+    boolean writeTypeOfCommit(int type) {
+        typeOfCommit = type;
+        return writeToArray(typeOfCommit, bitsPerType);
+    }
+
+    void readTypeOfCommit() {
+        typeOfCommit = readFromArray(headerLen + schemaLen, bitsPerType);
+    }
+
+}
