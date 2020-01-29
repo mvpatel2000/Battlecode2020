ae1a109b077774439dfe53cfa83f6893cc16dfee
Mihir <mihir.v.patel7@gmail.com>
2020-01-29 14:52:05 -0800

    fix drone prod

--- src/qual/FulfillmentCenter.java
+++ src/qual/FulfillmentCenter.java
@@ -71,7 +71,9 @@ public class FulfillmentCenter extends Building {
                 }
             } else if (!enemyAggression && rc.getRoundNum() > 200 && attackDroneCount + defenseDroneCount < 4) {
                 buildDrone();
-            } if (rc.getTeamSoup() >= 521 && rc.getRoundNum() > 655 && attackDroneCount + defenseDroneCount < 60 || rc.getTeamSoup() > 1100) {
+            } else if (!enemyAggression && rc.getRoundNum() > 350 && attackDroneCount + defenseDroneCount < 6) {
+                buildDrone();
+            } if (rc.getTeamSoup() >= 521 && rc.getRoundNum() > 655 && attackDroneCount + defenseDroneCount < 40 || rc.getTeamSoup() > 1100) {
                 buildDrone();
             }
         }
